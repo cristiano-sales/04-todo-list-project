1a60cfe430d8a50418c3217732cb1784efa4c9cf
Cristiano Sales <cristianosalesneves@gmail.com>
2021-11-24 09:46:56 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('#texto-tarefa');
+
+
+button.addEventListener('click', function(e) { //Função para resgatar o valor do formulário
+    e.preventDefault(); //preventDefault para não enviar o formulário
+
+})
